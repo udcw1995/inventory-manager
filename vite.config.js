71945661e391b7241c6566b019344f2cb5fb8e08
UDCW <udcwickramarathna@gmail.com>
2025-08-24 17:36:40 +0530

    vite fix

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,18 @@
-import { defineConfig } from "vite";
-import laravel from "laravel-vite-plugin";
-import tailwindcss from "@tailwindcss/vite";
+import { defineConfig } from 'vite';
+import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
     plugins: [
         laravel({
-            input: ["resources/css/app.css", "resources/js/app.js"],
-            refresh: [...refreshPaths, "app/Livewire/**", "app/Filament/**"],
+            input: [
+                'resources/css/app.css',
+                'resources/js/app.js',
+            ],
+            refresh: [
+                'resources/views/**',
+                'routes/**',
+                'app/Http/Livewire/**',
+            ],
         }),
-        tailwindcss(),
     ],
 });
